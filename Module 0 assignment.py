a3e38e60ea378bce448dfd9a625ef0c1de5dd63c
Luis Zurita <zlazaro2606@gmail.com>
2025-07-07 19:18:28 -0700

    Initial commit

---
+++ Module 0 assignment.py
@@ -0,0 +1,10 @@
+print('Luis Zurita')
+print()
+print('CSC 142 - Computer Programmig I')
+print()
+print(f"""One thing I learned from the module 0:
+I did not now about GitHub and GitBush to upload code as repository.
+Totally no idea, I'm starting this coding pathway""")
+
+
+
