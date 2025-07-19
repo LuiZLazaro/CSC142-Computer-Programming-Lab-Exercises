711cd4b33d1836c7360cd9122f62e00c9f155d72
LuisZLazaro <zlazaro2606@gmail.com>
2025-07-18 23:05:34 -0700

    Lab0 Java using Eclipse

---
+++ Lab0/src/lab0/Main.java
@@ -0,0 +1,12 @@
+package lab0;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CSC 142 - Java");
+		System.out.println("I learned the different for loop syntax.");
+
+	}
+
+}
